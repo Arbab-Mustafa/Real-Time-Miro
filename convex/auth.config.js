6a1f879d6c64f92df213d9be15cf12e7a15517fd
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-07 01:38:13 +0500

    add authentication

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const config = {
+  providers: [
+    {
+      domain: "https://promoted-macaw-86.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default config;
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const ConvexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(ConvexUrl);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
