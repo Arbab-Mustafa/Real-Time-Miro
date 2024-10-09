084eb3a008ab60821a445968f1ee2d3b268c98d0
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-09 15:26:52 +0500

    add new functinality in Organization component

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5  ">
       <div className="hidden lg:flex lg:flex-1 "></div>
 
       <UserButton />
