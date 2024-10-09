f8b00234b009b1499c1624e120e1dd8761ab4b43
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-09 14:12:47 +0500

    add new functinality in Organization component

--- app/(dashboard)/_components/OrgSidebar.tsx
+++ app/(dashboard)/_components/OrgSidebar.tsx
@@ -1,10 +1,86 @@
 "use client";
 import React from "react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+// import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard, Star } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
+const font = Poppins({
+  subsets: ["latin"],
+
+  weight: ["600"],
+});
 
 const OrgSidebar = () => {
+  const searchParams = useSearchParams();
+  const favorites = searchParams.get("favorites");
+
   return (
-    <div className="hidden md:flex flex-col  bg-red-500 space-y-6 pl-5 pt-5 w-[206px]">
-      OrgSidebar
+    <div className="hidden md:flex flex-col    space-y-6 pl-5 pt-5 w-[206px]">
+      <Link href="/">
+        <div className="flex items-center  gap-x-2">
+          {/* <Image src="" alt="logo" height={60} width={60} /> */}
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyItems: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+
+      <div className="space-y-1 w-full">
+        <Button
+          variant={favorites ? "ghost" : "secondary"}
+          asChild
+          size="lg"
+          className="font-normal justify-start  px-2  w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4  mr-2" />
+            Team boards
+          </Link>
+        </Button>
+        <Button
+          variant={favorites ? "secondary" : "ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start  px-2  w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: { favorites: true },
+            }}
+          >
+            <Star className="h-4 w-4  mr-2" />
+            Fav boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1 bg-black"></div>
+      <div className="hidden lg:flex lg:flex-1 "></div>
 
       <UserButton />
     </div>
