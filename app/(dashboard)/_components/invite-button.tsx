6e2260282864c17bcd45e8ad6706c65af3c5dcfe
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-12 20:01:34 +0500

    add invite button

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+
+const OrgInviteBtn = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="" variant="outline">
+          {" "}
+          <Plus className="w-4 h-4 mr-2" /> Invite Member
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[880px] mx-auto">
+        <OrganizationProfile />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default OrgInviteBtn;
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,15 +1,45 @@
 "use client";
-import { UserButton } from "@clerk/clerk-react";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
 
 import SearchInput from "./search-input";
+import OrgInviteBtn from "./invite-button";
 
 const Navbar = () => {
+  const { organization } = useOrganization();
   return (
     <div className="flex items-center gap-x-4 p-5  ">
       <div className="hidden lg:flex lg:flex-1 ">
         <SearchInput />
       </div>
-      <div className="block"></div>
+      <div className="block lg:hidden  flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyItems: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+      {organization && <OrgInviteBtn />}
 
       <UserButton />
     </div>
