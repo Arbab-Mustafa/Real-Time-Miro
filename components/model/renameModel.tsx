bd8566ac50df177767dc33ec3c7737ed7f842f48
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-16 15:57:32 +0500

    add edit model window

--- components/model/renameModel.tsx
+++ components/model/renameModel.tsx
@@ -1,17 +1,43 @@
 "use client";
-import React from "react";
+import React, { FormEventHandler, useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { useRenameModel } from "@/store/useRenameModel";
+import { Input } from "../ui/input";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Button } from "../ui/button";
+import useMutationAip from "@/hooks/useMutationAip";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const RenameModel = () => {
+  const { mutate, pending } = useMutationAip(api.board.update);
   const { isOpen, onClose, initialValues } = useRenameModel();
+  const [title, setTitle] = useState(initialValues.title);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    mutate({ id: initialValues.id, title })
+      .then(() => {
+        toast.success("Board title updated !");
+        onClose();
+      })
+      .catch(() => {
+        toast.error("Failed to update board title !");
+      });
+  };
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
@@ -19,6 +45,29 @@ const RenameModel = () => {
           <DialogTitle>Edit Board Title</DialogTitle>
           <DialogDescription>Enter New Title</DialogDescription>
         </DialogHeader>
+        <DialogDescription>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <Input
+              disabled={pending}
+              required
+              maxLength={40}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Board Title"
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="outline">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="submit" disabled={false}>
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
