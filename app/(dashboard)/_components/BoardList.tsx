7673d0169efadb8a8f4bfd754385126742e9bc5e
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-14 01:36:25 +0500

    add searching Functionality

--- app/(dashboard)/_components/BoardList.tsx
+++ app/(dashboard)/_components/BoardList.tsx
@@ -17,7 +17,7 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = useQuery(api.boards.get, { orgId, search: query.search });
+  const data = useQuery(api.boards.get, { orgId, ...query });
 
   if (data === undefined) {
     return (
--- convex/boards.ts
+++ convex/boards.ts
@@ -1,16 +1,33 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { getAllOrThrow } from "convex-helpers/server/relationships";
 
 export const get = query({
   args: {
     orgId: v.string(),
     search: v.optional(v.string()),
+    favorite: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
       throw new Error("Unauthorized");
+    }
+
+    if (args.favorite) {
+      const favoriteBoards = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_org", (q) =>
+          q.eq("userId", identity.subject).eq("orgId", args.orgId)
+        )
+        .order("desc")
+        .collect();
+
+      const ids = favoriteBoards.map((favorite) => favorite.boardId);
+      const boards = await getAllOrThrow(ctx.db, ids);
+
+      return boards.map((board) => ({ ...board, isFavorite: true }));
     }
 
     const title = args.search ? args.search : "";
