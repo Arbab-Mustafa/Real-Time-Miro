cfe4d9c6978fc4ac7895cde6f3a7a7bfa3ad63dc
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-16 19:44:00 +0500

    added Authentication

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = [
   "/public/1.jpg",
@@ -157,5 +157,12 @@ export const unFavorite = mutation({
     await ctx.db.delete(existingFavorite._id);
 
     return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    return ctx.db.get(args.id);
   },
 });
