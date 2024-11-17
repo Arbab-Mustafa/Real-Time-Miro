44248feb08d0b977e2810e1530bbba669d72481d
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-17 21:32:38 +0500

    add room logo pop Up

--- app/board/_components/info.tsx
+++ app/board/_components/info.tsx
@@ -1,11 +1,15 @@
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { Id } from "../../../convex/_generated/dataModel";
-import Image from "next/image";
-// import { Poppins } from "next/font/google";
+// import Image from "next/image";
+import { Poppins } from "next/font/google";
 
-// import { cn } from "../../../lib/utils";
+import { cn } from "../../../lib/utils";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Hint from "@/components/hint";
+
+import { useRenameModel } from "@/store/useRenameModel";
 
 //
 interface BoardInfoProps {
@@ -13,21 +17,49 @@ interface BoardInfoProps {
 }
 //
 
-// const font = Poppins({
-//   subsets: ["latin"],
+const font = Poppins({
+  subsets: ["latin"],
 
-//   weight: ["600"],
-// });
+  weight: ["600"],
+});
+
+//
+
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5"> | </div>;
+};
+
 const BoardInfo = ({ boardId }: BoardInfoProps) => {
+  const { onOpen } = useRenameModel();
   const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
 
   if (!data) return <p>Null </p>;
 
   return (
     <div className="absolute  top-2 left-2 bg-white  rounded-md px-1.5 h-12 shadow-sm flex items-center">
-      <Button className="px-2">
-        {/* <Image src="/logo.svg" alt="" height={40} width={40} /> */}
-        <p>MIRO</p>
+      <Hint label="go to board" side="bottom" sideOffset={10}>
+        <Link href="/">
+          <Button className="px-2" variant="board">
+            {/* <Image src="/logo.svg" alt="" height={40} width={40} /> */}
+            <p
+              className={cn(
+                "font-semibold  text-xl ml-2 text-black ",
+                font.className
+              )}
+            >
+              MIRO
+            </p>
+          </Button>
+        </Link>
+      </Hint>
+
+      <TabSeparator />
+      <Button
+        variant="board"
+        className="text-base px-2 font-normal"
+        onClick={() => onOpen(data?._id, data.title)}
+      >
+        {data.title}
       </Button>
     </div>
   );
