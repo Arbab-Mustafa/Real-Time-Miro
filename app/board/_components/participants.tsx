24d85d137f1fd8a8405e4474a0dd3914ae9a4836
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-18 14:51:42 +0500

    adding Active Participents information

--- app/board/_components/participants.tsx
+++ app/board/_components/participants.tsx
@@ -1,9 +1,7 @@
- 
-
 const Participants = () => {
   return (
     <div className="absolute  top-2 right-2 bg-white  rounded-md p-3 h-12 shadow-sm flex items-center">
-      Contact Information
+      {/* Contact Information */}
     </div>
   );
 };
@@ -13,6 +11,5 @@ export default Participants;
 Participants.Skeleton = function ParticipantsSkeleton() {
   return (
     <div className="absolute  top-2 right-2 bg-white  rounded-md p-3 h-12 shadow-sm flex items-center w-[100px] " />
-       
   );
 };
