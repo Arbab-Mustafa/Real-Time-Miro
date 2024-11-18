5d6f09d0128ec67b2c00c53fa68953ef3cb63e9f
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-18 23:03:52 +0500

    add sucessfully  Toolbar

---
+++ app/board/_components/ToolButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Hint from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { LucideIcon } from "lucide-react";
+
+interface ToolButtonProps {
+  label: string;
+  icon: LucideIcon;
+  onClick: () => void;
+  isActive?: boolean;
+  isDisabled?: boolean;
+}
+
+const ToolBtn = ({
+  label,
+  icon: Icon,
+  onClick,
+  isActive,
+  isDisabled,
+}: ToolButtonProps) => {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  );
+};
+
+export default ToolBtn;
--- app/board/_components/toolbar.tsx
+++ app/board/_components/toolbar.tsx
@@ -1,16 +1,65 @@
 import React from "react";
+import ToolBtn from "./ToolButton";
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
 
 const Toolbar = () => {
   return (
     <div className="flex flex-col gap-y-4 absolute top-[50%] -translate-y-[50%]">
       <div className="flex flex-col bg-white rounded-md gap-y-1 items-center shadow-md">
-        <div>Pencil</div>
-        <div>Square</div>
-        <div>Circle</div>
+        <ToolBtn
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolBtn label="Text" icon={Type} onClick={() => {}} isActive={false} />
+        <ToolBtn
+          label="Sticky note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolBtn
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolBtn
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolBtn
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="flex flex-col bg-white rounded-md p-1.5 items-center shadow-md">
-        <div>Undo</div>
-        {/* <div>Redo</div> */}
+        <ToolBtn
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolBtn
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
     </div>
   );
